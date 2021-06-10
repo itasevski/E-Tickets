6b76648196ea1a0501b29d90bc110407f28c67cc
Ivo Tasevski <ivo_t@live.com>
2021-06-10 21:18:45 +0200

    commit 1

---
+++ ETicketsApplication/ETickets.Web/Models/Domain/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using ETickets.Web.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETickets.Web.Models.Domain
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string OwnerId { get; set; }
+        [Required]
+        public ETicketsApplicationUser Owner { get; set; }
+        [Required]
+        public virtual ICollection<TicketInShoppingCart> TicketsInShoppingCart { get; set; }
+    }
+}
---
+++ ETicketsApplication/ETickets.Web/Models/Domain/TicketInShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETickets.Web.Models.Domain
+{
+    public class TicketInShoppingCart
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid TicketId { get; set; }
+        public Ticket Ticket { get; set; }
+        public Guid ShoppingCartId { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
